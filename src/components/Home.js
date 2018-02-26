772a02f80d8a3db559b22967b9f7a340621201b1
Eli <elicohen2018@u.northwestern.edu>
2018-02-26 00:45:39 -0600

    tweaked home page text

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,15 @@ export default class Home extends React.Component {
   render() {
     return (
       <div className="page-home">
-        Try going to https://b-2018.firebaseapp.com/1
+        Try going to:
+        <br></br>
+        <a href="https://b-2018.firebaseapp.com/a">https://b-2018.firebaseapp.com/a</a>
+        <br></br>
+        <a href="https://b-2018.firebaseapp.com/b">https://b-2018.firebaseapp.com/b</a>
+        <br></br>
+        <a href="https://b-2018.firebaseapp.com/c">https://b-2018.firebaseapp.com/c</a>
+        <br></br>
+        for our 3 demos!
       </div>
     );
   }
